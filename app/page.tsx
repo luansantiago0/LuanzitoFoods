905227605a2a8cd98578a646d35392d767353bf4
luansantiago0 <luansantiago0200@gmail.com>
2024-04-30 15:31:17 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import SearchInput from "./_components/search";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -11,6 +12,18 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/Banner Pizza.svg"
+          alt="Até 30% de descontos em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
